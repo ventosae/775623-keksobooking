f578d04b8cfe3c9493ea5c1d2ae0d53304a83b7e
ventosae <anton.surov@hotmail.com>
2018-09-25 23:39:16 +1000

    addPin fucntion added

--- js/map.js
+++ js/map.js
@@ -1,76 +1,111 @@
-var mapData = {
-  "author": {
-      avatar: [],
-      generateAvatars: function() {
-          for (var i = 0; i < AVATARS_NUMBER; i++) {
-              mapData.author.avatar.push('img/avatars/user0' + 'i' + '.png')
-          }
-      }
-  },
-  titles: [
-      'Большая уютная квартира',
-      'Маленькая неуютная квартира',
-      'Огромный прекрасный дворец',
-      'Маленький ужасный дворец',
-      'Красивый гостевой домик',
-      'Некрасивый негостеприимный домик',
-      'Уютное бунгало далеко от моря',
-      'Неуютное бунгало по колено в воде'
-    ],
-    price: {
-      min: 1000,
-      max: 1000000
+   'use strict';
+AVATARS_NUMBER = 8;
+
+var fullMap = document.querySelector('.map');
+
+  var mapData = {
+    "author": {
+        avatar: [],
     },
-    types: [
-      'palace',
-      'flat',
-      'house',
-      'bungalo'
-    ],
-    rooms: {
-      min: 1,
-      max: 5
-    },
-    guests: {
-      min: 1,
-      max: 3
-    },
-    checkin: [
-      '12:00',
-      '13:00',
-      '14:00'
-    ],
-    checkout: [
-      '12:00',
-      '13:00',
-      '14:00'
-    ],
-    features: [
-      'wifi',
-      'dishwasher',
-      'parking',
-      'washer',
-      'elevator',
-      'conditioner'
-    ],
-    photos: [
-      'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-      'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-      'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
-    ],
-    locations: {
-      x: {
-        min: 0,
-        max: map.offsetWidth
+    titles: [
+        'Большая уютная квартира',
+        'Маленькая неуютная квартира',
+        'Огромный прекрасный дворец',
+        'Маленький ужасный дворец',
+        'Красивый гостевой домик',
+        'Некрасивый негостеприимный домик',
+        'Уютное бунгало далеко от моря',
+        'Неуютное бунгало по колено в воде'
+      ],
+      price: {
+        min: 1000,
+        max: 1000000
       },
-      y: {
-        min: 130,
-        max: 630
+      types: [
+        'palace',
+        'flat',
+        'house',
+        'bungalo'
+      ],
+      rooms: {
+        min: 1,
+        max: 5
+      },
+      guests: {
+        min: 1,
+        max: 3
+      },
+      checkin: [
+        '12:00',
+        '13:00',
+        '14:00'
+      ],
+      checkout: [
+        '12:00',
+        '13:00',
+        '14:00'
+      ],
+      features: [
+        'wifi',
+        'dishwasher',
+        'parking',
+        'washer',
+        'elevator',
+        'conditioner'
+      ],
+      photos: [
+        'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+        'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+        'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+      ],
+      locations: {
+        x: {
+          min: 0,
+          max: fullMap.offsetWidth
+        },
+        y: {
+          min: 130,
+          max: 630
+        }
       }
-    }
-  };
+    };
+
+    var generateAvatars = function() {
+      for (var i = 1; i < AVATARS_NUMBER; i++) {
+          mapData.author.avatar.push('img/avatars/user0' + i + '.png')
+      }
+  }
+  generateAvatars()
 
 // Пишем рандомизатор
-var getRandomNumber = function (min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  var getRandomElement = function (arrayName) {
+    return arrayName[Math.floor(Math.random() * (arrayName.length))];
+  };
+
+// Get random number
+  var getRandomNumber = function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+
+document.querySelector('.map').classList.remove('map--faded');
+var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var mapPinBase = document.querySelector('.map__pins');
+var fragment = document.createDocumentFragment();
+
+
+
+
+var addPin = function() {
+  var thePin = mapPinTemplate.cloneNode(true);
+  var randomX = mapPinTemplate.offsetWidth + getRandomNumber(mapData.locations.x.min, mapData.locations.x.max);
+  var randomY = mapPinTemplate.offsetWidth + getRandomNumber(mapData.locations.y.min, mapData.locations.y.max);
+  var srcPin = getRandomElement(mapData.author.avatar);
+  var altPin = getRandomElement(mapData.titles);
+  thePin.querySelector('img').src = srcPin;
+  thePin.querySelector('img').alt = altPin;
+  thePin.style.left =  randomX + 'px';
+  thePin.style.top = randomY + 'px';
+  fragment.appendChild(thePin);
+  mapPinBase.appendChild(fragment)
 };
